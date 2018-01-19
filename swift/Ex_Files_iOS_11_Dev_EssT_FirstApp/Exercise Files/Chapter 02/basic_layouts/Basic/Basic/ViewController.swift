54ad0c8a7a461d4e6237afd3449378e47b5f5646
Nix <yiunixon@gmail.com>
2018-01-18 17:56:00 -0800

    handle button press

--- swift/Ex_Files_iOS_11_Dev_EssT_FirstApp/Exercise Files/Chapter 02/basic_layouts/Basic/Basic/ViewController.swift
+++ swift/Ex_Files_iOS_11_Dev_EssT_FirstApp/Exercise Files/Chapter 02/basic_layouts/Basic/Basic/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBAction func didTapLoad(_ sender: Any) {
+        myLabel.text = "changed with code"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
